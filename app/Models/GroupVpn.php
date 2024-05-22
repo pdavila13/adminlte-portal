cacc1d2652ed49161bcbbd5c18f23b3d98581e95
Paolo Davila <paolo.davilab@gmail.com>
2024-05-22 09:57:23 +0200

    Added GroupVpn Model with its migration and relationship to company

--- app/Models/GroupVpn.php
+++ app/Models/GroupVpn.php
@@ -2,17 +2,17 @@
 
 namespace App\Models;
 
-use App\Models\GroupVpn;
+use App\Models\Company;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class Company extends Model
+class GroupVpn extends Model
 {
     use HasFactory;
 
-    public function groupVpn(): BelongsTo
+    public function company() : BelongsTo
     {
-        return $this->belongsTo(GroupVpn::class);
+        return $this->belongsTo(Company::class);
     }
 }
---
+++ database/migrations/2024_05_22_075243_create_group_vpns_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('group_vpns', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('slug');
+            $table->string('network')->nullble();
+            $table->text('description')->nullble();
+            $table->foreignIdFor(Company::class)->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('group_vpns');
+    }
+};
