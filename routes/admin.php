5cbdc9d441f4a13b18d33776e6dd64df40abb640
Paolo Davila <paolo.davilab@gmail.com>
2024-05-22 09:48:26 +0200

    Move HomeController and home view in directory Admin and change routes

--- routes/admin.php
+++ routes/admin.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\HomeController;
 
-Route::get('', function () {
-    return "Administrator";
-});
+Route::get('', [HomeController::class, 'index'])->name('home');
