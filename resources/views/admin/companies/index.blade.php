51f0691532fa1cc08fab80a7326149b021fe45ce
Paolo Davila <paolo.davilab@gmail.com>
2024-06-02 09:16:36 +0200

    Added Index view for Companies

--- resources/views/admin/companies/index.blade.php
+++ resources/views/admin/companies/index.blade.php
@@ -1,43 +1,55 @@
 @extends('adminlte::page')
 
-@section('title', 'List company')
+@section('title', 'Companies')
 
 @section('content_header')
-    <h1>List company</h1>
+    <h1>Companies</h1>
 @stop
 
 @section('content')
+    @php
+        $heads = [
+            ['label' => 'ID', 'width' => 5],
+            'Name',
+            ['label' => 'Actions', 'no-export' => true, 'width' => 5],
+        ];
+
+        $config = [
+        'order' => [[1, 'des']],
+        'columns' => [null, null, null, ['orderable' => false]],
+        ];
+    @endphp
+
     <div class="card">
         <div class="card-header">
-            <a class="btn btn-secondary" href="{{ route('admin.companies.create')}}">Add</a>
+            <h3 class="card-title">List companies</h3>
+            <div class="card-tools">
+                <a class="btn btn-primary btn-sm" href="{{ route('admin.companies.create')}}"><i class="fas fa-plus"></i></a>
+            </div>
         </div>
-
-        <div class="car-body">
-            <table class="table table-striped">
-                <thead>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th colspan="2"></th>
-                </thead>
-                <tbody>
-                    @foreach ($companies as $company)
-                        <tr>
-                            <td>{{ $company->id }}</td>
-                            <td>{{ $company->name }}</td>
-                            <td width="10px">
-                                <a class="btn btn-primary btn-xs" href="{{ route('admin.companies.edit', $company)}}">Edit</a>
-                            </td>
-                            <td width="10px">
-                                <form action="{{ route('admin.companies.destroy', $company)}}" method="POST">
-                                    @csrf
-                                    @method('delete')
-                                    <button type="submit" class="btn btn-danger btn-xs">Delete</button>
-                                </form>
-                            </td>
-                        </tr>
-                    @endforeach
-                </tbody>
-            </table>
+        <div class="card-body">
+            <x-adminlte-datatable id="table2" :heads="$heads">
+                @foreach ($companies as $company)
+                    <tr>
+                        <td>{{ $company->id }}</td>
+                        <td>{{ $company->name }}</td>
+                        <td class="text-right">
+                            <a href="{{ route('admin.companies.edit', $company) }}" class="btn btn-info btn-xs">
+                                <i class="fas fa-edit"></i>
+                            </a>
+                            <form action="{{ route('admin.companies.destroy', $company) }}" method="POST" onsubmit="return confirm('{{ __('Are sure want to delete?') }}')" style="display: inline;">
+                                @method('DELETE')
+                                @csrf
+                                <button type="submit" class="btn btn-danger btn-xs">
+                                    <i class="fas fa-trash"></i>
+                                </button>
+                            </form>
+                        </td>
+                    </tr>
+                @endforeach
+            </x-adminlte-datatable>
         </div>
     </div>
-@stop
+@endsection
+
+@section('plugins.Datatables', true)
