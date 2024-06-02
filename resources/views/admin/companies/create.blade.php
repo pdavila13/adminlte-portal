da82cecc8a2d8a427b28a9092643f8224c8819ff
Paolo Davila <paolo.davilab@gmail.com>
2024-06-02 21:26:41 +0200

    Install and use package laravel-spanish-validator

--- resources/views/admin/companies/create.blade.php
+++ resources/views/admin/companies/create.blade.php
@@ -3,30 +3,29 @@
     <x-adminlte-modal id="modalCompanyCreate" title="Create new company" theme="primary"
         icon="fas fa-plus" v-centered static-backdrop scrollable>
         <div class="row">
-            <x-adminlte-input name="iName" label="Company name" placeholder="" label-class="text-lightblue"
-            value="{{ old('iName') }}" fgroup-class="col-md-6" error-key>
+            <x-adminlte-input name="name" label="Company name" placeholder="" label-class="text-lightblue"
+            value="{{ old('name') }}" fgroup-class="col-md-6" error-key>
                 <x-slot name="prependSlot">
                     <div class="input-group-text">
                         <i class="fas fa-building text-lightblue"></i>
                     </div>
                 </x-slot>
+                @error('name')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
             </x-adminlte-input>
-            @error('iName')
-                <span class="text-danger">{{ $message }}</span>
-            @enderror
 
-            <x-adminlte-input name="iCIF" label="Tax identification code" placeholder="" label-class="text-lightblue"
-            value="{{ old('iCIF') }}" fgroup-class="col-md-6" error-key>
+            <x-adminlte-input name="cif" label="Tax identification code" placeholder="" label-class="text-lightblue"
+            value="{{ old('cif') }}" fgroup-class="col-md-6" error-key>
                 <x-slot name="prependSlot">
                     <div class="input-group-text">
                         <i class="fas fa-address-card text-lightblue"></i>
                     </div>
                 </x-slot>
+                @error('cif')
+                    <span class="text-danger">{{ $message }}</span>
+                @enderror
             </x-adminlte-input>
-
-            @error('iCIF')
-                <span class="text-danger">{{ $message }}</span>
-            @enderror
 
             <x-adminlte-textarea name="taDesc" label="Description" rows=5 label-class="text-lightblue"
                 igroup-size="sm" placeholder="Insert description..." fgroup-class="col-md-12" error-key>
